2e123dd7ae0adb7ce85b301b8bed2fc972c704af
MrAjay1021 <ajay.prasad1021@gmail.com>
2025-12-22 14:55:33 +0530

    aiResponse placeholder in neon

--- backend/src/scraper/scrape.js
+++ backend/src/scraper/scrape.js
@@ -1,20 +1,21 @@
 import axios from 'axios'
-import cheerio from 'cheerio'
+import * as cheerio from 'cheerio'
+
 
 export async function scrapeWebsite(url) {
   const res = await axios.get(url, { timeout: 10000 })
-  const html = res.data
+  const html = res.data // get HTML
 
-  const $ = cheerio.load(html)
-  let content = $('body').text()
-  content = content.replace(/\s+/g, ' ').trim()
+  const $ = cheerio.load(html) //Load into Cheerio
+  let content = $('body').text() //Extract text
+  content = content.replace(/\s+/g, ' ').trim() // Clean whitespace
 
   if (content.length < 50) {
-    return { success: false, error: 'Content too short' }
+    return { success: false, error: 'Content too short' } // if false
   }
 
   if (content.length > 20000) {
-    content = content.slice(0, 20000)
+    content = content.slice(0, 20000) // return truncate
   }
 
   return { success: true, content }
--- backend/src/worker/worker.js
+++ backend/src/worker/worker.js
@@ -3,34 +3,35 @@ import { eq } from 'drizzle-orm'
 import { redisConnection } from '../queue/connection.js'
 import { db } from '../db/connection.js'
 import { tasks } from '../db/schema.js'
+import { scrapeWebsite } from '../scraper/scrape.js'
 
 const worker = new Worker(
   'scrape-queue',
   async (job) => {
     const { id, url, question } = job.data
 
-    // mark PROCESSING
-    await db
-      .update(tasks)
-      .set({ status: 'PROCESSING' })
+    await db.update(tasks)
+      .set({ status: 'PROCESSING' }) // status processing at start
       .where(eq(tasks.id, id))
 
-    console.log('Processing job', id, url, question)
+    const result = await scrapeWebsite(url) // calling 
 
-    // fake AI work
-    const fakeResponse = 'Test'
+      // If !result.success update DB with status = 'FAILED' showing errorMessage = result.error and throw error
+    if (!result.success) {
+      await db.update(tasks)
+        .set({ status: 'FAILED', errorMessage: result.error })
+        .where(eq(tasks.id, id))
+      throw new Error(result.error)
+    }
 
-    // mark COMPLETED
+    const scrapedContent = result.content // if success store here
     await db
       .update(tasks)
       .set({
         status: 'COMPLETED',
-        aiResponse: fakeResponse,
+        aiResponse: 'Scraped OK (no AI yet)',
       })
       .where(eq(tasks.id, id))
-  },
-  {
-    connection: redisConnection,
-    concurrency: 3,
-  }
+      },
+  { connection: redisConnection, concurrency: 3 }
 )
